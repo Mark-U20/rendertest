611798545525858b2e1f6c3072cc1f0b3130abd3
mark <mtustby@gmail.com>
2025-02-04 20:10:14 -0600

    asd

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,8 @@
-// const forceDatabaseRefresh = false;
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-// import routes from './routes/index.js';
-// import { sequelize } from './models/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,10 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('../client/dist'));
 
 app.use(express.json());
-// app.use(routes);
 
-// sequelize.sync({force: forceDatabaseRefresh}).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
-// });
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('Press CTRL + C to stop the server');
+});
