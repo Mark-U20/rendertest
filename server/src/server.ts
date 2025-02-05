9d559ddc2058b5d7bea59ff7ca052ed5dfd2e7bd
mark <mtustby@gmail.com>
2025-02-04 20:21:54 -0600

    asd

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,17 +7,14 @@ import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Create an absolute path to the client/dist folder
-const clientBuildPath = path.resolve(__dirname, '../client/dist');
-
 // Serve static files from the React app
-app.use(express.static(clientBuildPath));
+app.use(express.static('../client/dist'));
 
 app.use(express.json());
 
 // Catch-all route to serve index.html for any requests that don't match a static file
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(clientBuildPath, 'index.html'));
+  res.sendFile(path.join('../client/dist/index.html'));
 });
 
 app.listen(PORT, () => {
