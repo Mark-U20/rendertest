7cd92f8467baffd3c2583480234f7ab896cb5a07
mark <mtustby@gmail.com>
2025-02-04 20:26:27 -0600

    asd

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,28 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get __dirname in an ES module context
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Build an absolute path to the client/dist folder
+const clientBuildPath = path.resolve(__dirname, '../../client/dist');
+
 // Serve static files from the React app
-app.use(express.static('../client/dist'));
+app.use(express.static(clientBuildPath));
 
 app.use(express.json());
 
 // Catch-all route to serve index.html for any requests that don't match a static file
-app.get('*', (_req, res) => {
-  res.sendFile(path.join('../client/dist/index.html'));
+app.get('*', (_req: Request, res: Response) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
